507403ad3e797358dc114dda2aebc970138a5302
Detlev-zetes <detlev.vanlooy@zetes.com>
2020-05-14 11:18:09 +0200

    Fix visual pdf nullpointer

--- signandvalidation-ws/src/main/java/com/zetes/projects/bosa/signandvalidation/service/DocumentSignatureService.java
+++ signandvalidation-ws/src/main/java/com/zetes/projects/bosa/signandvalidation/service/DocumentSignatureService.java
@@ -116,14 +116,16 @@ public class DocumentSignatureService extends AbstractRemoteSignatureServiceImpl
         PAdESSignatureParameters padesParams = new PAdESSignatureParameters();
         padesParams.setSignatureSize(9472 * 2); // double reserved space for signature
 
-        SignatureImageTextParameters textParameters = new SignatureImageTextParameters();
-        textParameters.setText(remoteParameters.getPdfSignatureFieldText());
+        if (remoteParameters.getPdfSignatureFieldId() != null && remoteParameters.getPdfSignatureFieldText() != null) {
+            SignatureImageTextParameters textParameters = new SignatureImageTextParameters();
+            textParameters.setText(remoteParameters.getPdfSignatureFieldText());
 
-        SignatureImageParameters imageParameters = new SignatureImageParameters();
-        imageParameters.setTextParameters(textParameters);
+            SignatureImageParameters imageParameters = new SignatureImageParameters();
+            imageParameters.setTextParameters(textParameters);
 
-        padesParams.setSignatureFieldId(remoteParameters.getPdfSignatureFieldId());
-        padesParams.setSignatureImageParameters(imageParameters);
+            padesParams.setSignatureFieldId(remoteParameters.getPdfSignatureFieldId());
+            padesParams.setSignatureImageParameters(imageParameters);
+        }
 
         return padesParams;
     }
