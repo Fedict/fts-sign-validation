9b3b258a5c5c85ccfcdad2b8dfe75e91448248a4
Christian <work@xylsys.com>
2022-12-22 16:33:07 +0100

    First draft

--- signandvalidation-ws/src/main/java/com/bosa/signandvalidation/service/BosaRemoteDocumentValidationService.java
+++ signandvalidation-ws/src/main/java/com/bosa/signandvalidation/service/BosaRemoteDocumentValidationService.java
@@ -1,22 +1,24 @@
 package com.bosa.signandvalidation.service;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
-import java.util.Date;
 import java.io.Serializable;
+
+import eu.europa.esig.dss.diagnostic.jaxb.XmlDistinguishedName;
 import eu.europa.esig.dss.enumerations.Indication;
 import eu.europa.esig.dss.enumerations.SubIndication;
 import eu.europa.esig.dss.enumerations.DigestAlgorithm;
 import eu.europa.esig.dss.enumerations.SignatureLevel;
-import eu.europa.esig.dss.simplereport.jaxb.XmlDetails;
-import eu.europa.esig.dss.simplereport.jaxb.XmlMessage;
+import eu.europa.esig.dss.simplereport.jaxb.*;
+import eu.europa.esig.dss.utils.Utils;
 import eu.europa.esig.dss.ws.dto.RemoteDocument;
 import eu.europa.esig.dss.ws.signature.dto.parameters.RemoteSignatureParameters;
 import eu.europa.esig.dss.ws.validation.dto.WSReportsDTO;
 import eu.europa.esig.dss.ws.validation.dto.DataToValidateDTO;
 import eu.europa.esig.dss.diagnostic.jaxb.XmlDiagnosticData;
 import eu.europa.esig.dss.diagnostic.jaxb.XmlBasicSignature;
-import eu.europa.esig.dss.simplereport.jaxb.XmlSimpleReport;
-import eu.europa.esig.dss.simplereport.jaxb.XmlToken;
+import eu.europa.esig.dss.diagnostic.jaxb.XmlSignature;
 import eu.europa.esig.dss.detailedreport.jaxb.XmlValidationProcessBasicSignature;
 import eu.europa.esig.dss.detailedreport.jaxb.XmlConclusion;
 
@@ -24,6 +26,7 @@ import eu.europa.esig.dss.detailedreport.jaxb.XmlConclusion;
  * This validation service calls the DSS validation service and then applies some extra checks.
  */
 public class BosaRemoteDocumentValidationService {
+	private static final String BRCA_3_CONSTRAINT_FILE = "BRCA3_constraint.xml";
 	private ShadowRemoteDocumentValidationService remoteDocumentValidationService;
 
 	public BosaRemoteDocumentValidationService() {
@@ -41,18 +44,26 @@ public class BosaRemoteDocumentValidationService {
 
 		// Let DSS do its normal validation
 		WSReportsDTO report = remoteDocumentValidationService.validateDocument(new DataToValidateDTO(signedDocument, originalDocuments, policy));
+		if (policy == null && hasBRCA3RevocationFreshnessError(report)) {
+			try {
+				InputStream brca3is = BosaRemoteDocumentValidationService.class.getResourceAsStream("/policy/" + BRCA_3_CONSTRAINT_FILE);
+				RemoteDocument brca3Policy = new RemoteDocument(Utils.toByteArray(brca3is), BRCA_3_CONSTRAINT_FILE);
+				report = remoteDocumentValidationService.validateDocument(new DataToValidateDTO(signedDocument, originalDocuments, brca3Policy));
+			} catch (IOException e) {
+				throw new RuntimeException(BRCA_3_CONSTRAINT_FILE + " not found");
+			}
+		}
 
 		// When some back end servers (don't know which ones...) are down seems DSS can produce a signature that does not reflect the
 		// requested "parameters.getSignatureLevel()". For example even though an LTA was requested the result is not LTA.
 		// The code below is there to double-check this, it also makes sure SHA1 & MD5 are never used
-		eu.europa.esig.dss.diagnostic.jaxb.XmlSignature maxSig = null;
 		XmlDiagnosticData diagsData = report.getDiagnosticData();
-		List<eu.europa.esig.dss.diagnostic.jaxb.XmlSignature> signatures = diagsData.getSignatures();
-
+		List<XmlSignature> signatures = diagsData.getSignatures();
 		int sigCount = signatures.size();
+		XmlSignature maxSig = null;
 		for (int i = 0; i < sigCount; i++) {
-			eu.europa.esig.dss.diagnostic.jaxb.XmlSignature sig = signatures.get(i);
-			// Check if the signature algo is MD5 or SHA1
+			XmlSignature sig = signatures.get(i);
+			// Check if the signature algo is MD5 or SHA1 and make it an error
 			XmlBasicSignature basicSig = sig.getBasicSignature();
 			DigestAlgorithm digestAlgo = basicSig.getDigestAlgoUsedToSignThisToken();
 			if (digestAlgo != null) {
@@ -81,6 +92,32 @@ public class BosaRemoteDocumentValidationService {
 		}
 
 		return report;
+	}
+
+	private static boolean hasBRCA3RevocationFreshnessError(WSReportsDTO report) {
+		for(XmlToken sigOrTS : report.getSimpleReport().getSignatureOrTimestamp()) {
+			if (sigOrTS instanceof eu.europa.esig.dss.simplereport.jaxb.XmlSignature &&
+					Indication.INDETERMINATE.equals(sigOrTS.getIndication()) &&
+					SubIndication.TRY_LATER.equals(sigOrTS.getSubIndication())) {
+				XmlCertificateChain certChain = sigOrTS.getCertificateChain();
+				if (certChain == null) continue;
+				List<XmlCertificate> certChain2 = certChain.getCertificate();
+				XmlCertificate rootCert = certChain2.get(certChain2.size() - 1);
+				if (isBRCA3Cert(report.getDiagnosticData().getUsedCertificates(), rootCert.getId())) return true;
+			}
+		}
+	return false;
+	}
+
+	private static boolean isBRCA3Cert(List<eu.europa.esig.dss.diagnostic.jaxb.XmlCertificate> certs, String id) {
+		for (eu.europa.esig.dss.diagnostic.jaxb.XmlCertificate cert : certs) {
+			if (id.equals(cert.getId())) {
+				for(XmlDistinguishedName formattedDn : cert.getSubjectDistinguishedName()) {
+					if ("CANONICAL".equals(formattedDn.getFormat()) && "cn=belgium root ca3,c=be".equals(formattedDn.getValue())) return true;
+				}
+			}
+		}
+		return false;
 	}
 
 	private void modifyReports(WSReportsDTO report, String sigId, SubIndication subIndication, String errMesg) {
