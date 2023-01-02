587e07f84bf41f4abda1ff458dd0fe2c3d4ce1e4
Christian <work@xylsys.com>
2023-01-02 15:06:29 +0100

    Log more and catch any exception

--- signandvalidation-ws/src/main/java/com/bosa/signandvalidation/job/AbortJob.java
+++ signandvalidation-ws/src/main/java/com/bosa/signandvalidation/job/AbortJob.java
@@ -1,15 +1,28 @@
 package com.bosa.signandvalidation.job;
 
 import com.bosa.signandvalidation.SignAndValidationApplication;
+import com.bosa.signandvalidation.controller.SigningController;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
 
 @Service
 public class AbortJob {
 
+    private static final Logger logger = Logger.getLogger(AbortJob.class.getName());
+
     @Scheduled(fixedDelay = 900000000, initialDelayString = "${shutdown.after.ms}")
     public void abort() {
-        SignAndValidationApplication.stop();
+        try {
+            logger.info("SignAndValidationApplication.stop");
+            SignAndValidationApplication.stop();
+        } catch(Exception e) {
+            logger.warning("SignAndValidationApplication.stop : Exception");
+            logger.warning(e.toString());
+            logger.warning(e.getMessage());
+        }
+        logger.info("System.exit");
         System.exit(0);
     }
 }
