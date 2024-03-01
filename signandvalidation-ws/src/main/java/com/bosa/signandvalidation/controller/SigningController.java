cc74d18929e7fa90bfc907027c73e2df1478bc91
chmo <chmo>
2024-03-01 11:08:48 +0100

    set NoSkipErrors to true for getTokenForDocument

--- signandvalidation-ws/src/main/java/com/bosa/signandvalidation/controller/SigningController.java
+++ signandvalidation-ws/src/main/java/com/bosa/signandvalidation/controller/SigningController.java
@@ -222,6 +222,7 @@ public class SigningController extends ControllerBase implements ErrorStrings {
             else pdfProfile = null;
 
             TokenObject token = new TokenObject(SigningType.Standard, tokenData.getName(), pdfProfile, xmlProfile, inputs, tokenData.getOut());
+            token.setNoSkipErrors(true);
             if (tokenData.getPolicyId() != null) {
                 token.setPolicy(new PolicyParameters(tokenData.getPolicyId(), tokenData.getPolicyDescription(), tokenData.getPolicyDigestAlgorithm()));
             }
@@ -299,8 +300,8 @@ public class SigningController extends ControllerBase implements ErrorStrings {
         token.setOutPathPrefix(gtfd.getOutPathPrefix());
         token.setRequestDocumentReadConfirm(gtfd.isRequestDocumentReadConfirm());
         token.setPreviewDocuments(gtfd.isPreviewDocuments());
-         token.setSelectDocuments(gtfd.isSelectDocuments());
-         token.setNoSkipErrors(gtfd.isNoSkipErrors());
+        token.setSelectDocuments(gtfd.isSelectDocuments());
+        token.setNoSkipErrors(gtfd.isNoSkipErrors());
 
         checkTokenAndSetDefaults(token);
 
