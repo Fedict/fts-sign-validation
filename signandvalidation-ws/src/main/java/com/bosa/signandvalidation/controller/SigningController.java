3ebb36a6f6fbad55e0aca5371cfcc4f2e8293089
chmo <chmo>
2024-05-06 17:06:22 +0200

    Rename profile XADES_JUSTACT_CITIZEN

--- signandvalidation-ws/src/main/java/com/bosa/signandvalidation/controller/SigningController.java
+++ signandvalidation-ws/src/main/java/com/bosa/signandvalidation/controller/SigningController.java
@@ -421,7 +421,7 @@ public class SigningController extends ControllerBase implements ErrorStrings {
         String prefix = token.getOutPathPrefix();
         if (SigningType.XadesMultiFile.equals(token.getSigningType())) {
             String profileId = token.getXmlSignProfile();
-            if (profileId == null || !profileId.startsWith("XADES_JUSTACCT_CITIZEN")) {
+            if (profileId == null || !profileId.startsWith("XADES_JUSTACT_CITIZEN")) {
                 logAndThrowEx(FORBIDDEN, INVALID_PARAM, "'Xades Multifile' with an invalid signProfile :" + token.getXmlSignProfile(), null);
             }
 
