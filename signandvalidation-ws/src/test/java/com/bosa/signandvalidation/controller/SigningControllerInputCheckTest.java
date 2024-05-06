ee7c00e0d48cb53d0a64003f655a1647b7d48e9a
chmo <chmo>
2024-05-06 16:31:01 +0200

    Ranme profile XADES_JUSTACCT_CITIZEN

--- signandvalidation-ws/src/test/java/com/bosa/signandvalidation/controller/SigningControllerInputCheckTest.java
+++ signandvalidation-ws/src/test/java/com/bosa/signandvalidation/controller/SigningControllerInputCheckTest.java
@@ -140,7 +140,7 @@ public class SigningControllerInputCheckTest {
 
         // finish with general params
         testToken(token, INVALID_PARAM, "'Xades Multifile' with an invalid signProfile :null");
-        token.setXmlSignProfile("MDOC_XADES_LTA");
+        token.setXmlSignProfile("XADES_JUSTACCT_CITIZEN");
 
         testToken(token, INVALID_PARAM, "'OutXslt' (file2.xml) is not unique");
         token.setOutXsltPath("OutXSLT.xml");
